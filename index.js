755d002e4e3af78bd4230c738ef7d783b20b54ff
lucasdmmc <lucas.dmmc@hotmail.com>
2023-04-27 16:14:11 -0300

    minor adjustments

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ import express from 'express'
 import cors from 'cors'
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 import { createSessionHandler } from './pages/api/createSession.js'
 
 import { getSessionResultHandler } from './pages/api/getSessionResult.js'
 
-app.get("/", (request, response) => {
-    response.send("Hi there");
-});
-
-app.get("/api/createSession", async (request, response) =>{
-    const result = await createSessionHandler(request, response)
+app.get("/api/createSession", async (request, response) => {
+    const sessionId = await createSessionHandler(request, response)
+    console.log(sessionId)
+    // response.json({ sessionId })
 })
 
-app.get("/api/getFaceLivenessResults", async (request, response) =>{
-    const result = await getSessionResultHandler(request, response)
+app.get("/api/getFaceLivenessResults", async (req, res) => {
+    const result = await getSessionResultHandler(req.query.sessionId);
+    console.log(result)
+    res.json(result)
 })
 
 app.listen(3000, () => {
