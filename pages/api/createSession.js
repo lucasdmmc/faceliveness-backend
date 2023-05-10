ec50caedeb011caf376180f54eae95d5b124200e
Ed Willy Pires <lawiletxd@live.com>
2023-05-10 11:42:05 -0300

    Adding try catch block for better debugging

--- pages/api/createSession.js
+++ pages/api/createSession.js
@@ -1,10 +1,21 @@
 import { getRekognitionClient } from '../../helpers/rekognition.js';
 
 export async function createSessionHandler(req, res) {
+  try {
     const rekognition = await getRekognitionClient();
-    const response = await rekognition.createFaceLivenessSession().then();
+    console.log('Creating FaceLiveness Session')
+    const { SessionId } = await rekognition.createFaceLivenessSession();
+    console.log('FaceLiveness Session was created successfully!')
+    res.status(200).json({ sessionId: SessionId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error! It was not possible to create a liveness session' });
+  }
+
+//   const rekognition = await getRekognitionClient();
+//     const response = rekognition.createFaceLivenessSession();
     
-    res.status(200).json({
-        sessionId: response.SessionId,
-    });
+//     res.status(200).json({
+//         sessionId: response.SessionId,
+//     });
 }
